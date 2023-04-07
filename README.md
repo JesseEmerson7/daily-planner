6bde214b219d639c6ba2c61bddb431a506447d41
Jesse Emerson <jesseemerson7@gmail.com>
2023-04-06 21:56:10 -0400

    got application running and colors changing, read me too

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # daily-planner
-A daily schedule that displays tasks to be done and tasks that have passed. Everything can be saved to local storage.
+
+A daily schedule that displays tasks to be done, tasks that have passed, and future tasks. Everything can be saved to local storage for the next time the page is viewed. This daily planner is meant to show tasks to be done during work hours(9am-5pm). Future tasks will show up as green, past tasks will show up as grey, and current tasks will show up as red and yellow. The current date and time are displayed at the top of the page.
+
+## Instructions and Usage
+
+No installation required.
+
+Once on the page enter in a task to be completed on the desired hour of the day. Then select the save button on the right side of the text area. Watch the changing colors to check what task is up next to complete.
+
+## view <a href="https://jesseemerson7.github.io/daily-planner/">Live page!</a>
+
+ <img src="./assets/images/127.0.0.1_5501_index.html.png">
+
+## License
+
+MIT license.
+
+## Contact
+
+Contact me via email at : <a href="mailto:jesseemerson7@gmail.com">jesseemerson7@gmail.coma</a>
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,9 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
-let hourNine = $("#hour-9");
-let hourTen = $("#hour-10");
-let hourEleven = $("#hour-11");
-let hourTwelve = $("#hour-12");
-let hourOne = $("#hour-1");
-let hourTwo = $("#hour-2");
-let hourThree = $("#hour-3");
-let hourFour = $("#hour-4");
-let hourFive = $("#hour-5");
 
 let currentTime = $("#currentDay");
+let todaysTime = dayjs().hour(13);
 
 $(document).ready(function () {
   currentTime.text("Todays Date and Time Here");
@@ -31,7 +23,6 @@ $(document).ready(function () {
   //putting saved data back onto the page if it has been saved using a .each method to loop through all "textarea" and find their id.
   $(".saveBtn").each(function () {
     let hourNumber2 = $(this).parent("div").attr("id");
-    // console.log(hourNumber2);
     //if the id saved in local storage matches the id of text area the info is put on the page if not it is left empty
     let localInput = localStorage.getItem(hourNumber2);
 
@@ -40,14 +31,83 @@ $(document).ready(function () {
     }
   });
 
-  //time at top of current time
+  //time at top of page
   setInterval(function () {
     $("#currentDay").text(dayjs().format("MMMM DD, YYYY, hh: mm: ss a"));
   }, 1000);
 
   //setting color classes to days past present and future
 
-  function colors() {}
+  function colors() {
+    hourNine = dayjs().hour(9);
+    hourTen = dayjs().hour(10);
+    hourEleven = dayjs().hour(11);
+    hourTwelve = dayjs().hour(12);
+    hourOne = dayjs().hour(13);
+    hourTwo = dayjs().hour(14);
+    hourThree = dayjs().hour(15);
+    hourFour = dayjs().hour(16);
+    hourFive = dayjs().hour(17);
+    console.log(hourNine);
+//9
+    if(hourNine.isAfter(todaysTime, 'hour')){
+      console.log('its working')
+      $('#hour-9').addClass('future')
+    }else if (hourNine.isSame(todaysTime, 'hour')){
+      $('#hour-9').addClass('present');
+    }else{ $('#hour-9').addClass('past')};
+//10
+    if(hourTen.isAfter(todaysTime, 'hour')){
+      $('#hour-10').addClass('future')
+      }else if (hourTen.isSame(todaysTime, 'hour')){
+      $('#hour-10').addClass('present');
+    }else{ $('#hour-10').addClass('past')};
+  //11
+  if(hourEleven.isAfter(todaysTime, 'hour')){
+    $('#hour-11').addClass('future')
+    }else if (hourEleven.isSame(todaysTime, 'hour')){
+    $('#hour-11').addClass('present');
+  }else{ $('#hour-11').addClass('past')};
+  //12
+  if(hourTwelve.isAfter(todaysTime, 'hour')){
+    $('#hour-12').addClass('future')
+    }else if (hourTwelve.isSame(todaysTime, 'hour')){
+    $('#hour-12').addClass('present');
+  }else{ $('#hour-12').addClass('past')};
+  //1
+  if(hourOne.isAfter(todaysTime, 'hour')){
+    $('#hour-1').addClass('future')
+    }else if (hourOne.isSame(todaysTime,'hour')){
+    $('#hour-1').addClass('present');
+  }else{ $('#hour-1').addClass('past')};
+  //2
+  if(hourTwo.isAfter(todaysTime, 'hour')){
+    $('#hour-2').addClass('future')
+    }else if (hourTwo.isSame(todaysTime,'hour')){
+    $('#hour-2').addClass('present');
+  }else{ $('#hour-2').addClass('past')};
+  //3
+  if(hourThree.isAfter(todaysTime, 'hour')){
+    $('#hour-3').addClass('future')
+    }else if (hourThree.isSame(todaysTime,'hour')){
+    $('#hour-3').addClass('present');
+  }else{ $('#hour-3').addClass('past')};
+  //4
+  if(hourFour.isAfter(todaysTime, 'hour')){
+    $('#hour-4').addClass('future')
+    }else if (hourFour.isSame(todaysTime,'hour')){
+    $('#hour-4').addClass('present');
+  }else{ $('#hour-4').addClass('past')};
+  //5
+  if(hourFive.isAfter(todaysTime, 'hour')){
+    $('#hour-5').addClass('future')
+    }else if (hourFive.isSame(todaysTime,'hour')){
+    $('#hour-5').addClass('present');
+  }else{ $('#hour-5').addClass('past')}
+
+  //long way to do it. Not sure how to otherwise.
+  }
+  colors();
 });
 
 // TODO: Add a listener for click events on the save button. This code should
